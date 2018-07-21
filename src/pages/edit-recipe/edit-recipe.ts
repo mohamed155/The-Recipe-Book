ffaa17cf712b29dbcc317e7e0dd75df3090f8723
Mohamed Ramadan <mohamed_ramadan154@hotmail.com>
2018-07-21 20:30:10 +0200

    Create and prepare required pages

---
+++ src/pages/edit-recipe/edit-recipe.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-edit-recipe',
+  templateUrl: 'edit-recipe.html',
+})
+export class EditRecipePage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad EditRecipePage');
+  }
+
+}
