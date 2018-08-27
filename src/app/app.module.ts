e72e75bfd1821436c02a10112b21af383a671f56
mohamed155 <mohamed_ramadan154@hotmail.com>
2018-08-27 15:23:22 +0200

    Add Signin&signup&sidemenu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,8 @@ import {ShoppingListPage} from "../pages/shopping-list/shopping-list";
 import {TabsPage} from "../pages/tabs/tabs";
 import {ShoppingListService} from "../services/shopping-list";
 import {RecipesService} from "../services/recipes";
+import {SigninPage} from "../pages/signin/signin";
+import {SignupPage} from "../pages/signup/signup";
 
 @NgModule({
   declarations: [
@@ -20,7 +22,9 @@ import {RecipesService} from "../services/recipes";
     RecipePage,
     RecipesPage,
     ShoppingListPage,
-    TabsPage
+    TabsPage,
+    SigninPage,
+    SignupPage
   ],
   imports: [
     BrowserModule,
@@ -33,7 +37,9 @@ import {RecipesService} from "../services/recipes";
     RecipePage,
     RecipesPage,
     ShoppingListPage,
-    TabsPage
+    TabsPage,
+    SigninPage,
+    SignupPage
   ],
   providers: [
     StatusBar,
---
+++ src/pages/signup/signup.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+import {NgForm} from "@angular/forms";
+
+@Component({
+  selector: 'page-signup',
+  templateUrl: 'signup.html',
+})
+export class SignupPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  onSignup(form: NgForm) {
+    console.log(form.value);
+  }
+
+}
