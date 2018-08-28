e0309f45f526c47fadc99d3e3893914ffce34920
mohamed155 <mohamed_ramadan154@hotmail.com>
2018-08-28 11:56:42 +0200

    Integrate Firebase API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import {Component, ViewChild} from '@angular/core';
 import {MenuController, NavController, Platform} from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import firebase from 'firebase';
 
 import {TabsPage} from "../pages/tabs/tabs";
 import {SigninPage} from "../pages/signin/signin";
@@ -17,6 +18,10 @@ export class MyApp {
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,
               public menuCtrl: MenuController) {
+    firebase.initializeApp({
+      apiKey: "AIzaSyBc_uDEGpefgSFfOVs6bLLTyQLXwLr41O8",
+      authDomain: "recipe-book-65ff1.firebaseapp.com"
+    });
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
