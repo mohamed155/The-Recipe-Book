062be5587b5ee0ef51641011881d8988b5e54c5e
mohamed155 <mohamed_ramadan154@hotmail.com>
2018-08-28 12:56:36 +0200

    Add signup with firebase

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import {NgForm} from "@angular/forms";
+import {AuthService} from "../../services/auth";
 
 @Component({
   selector: 'page-signup',
@@ -8,11 +9,15 @@ import {NgForm} from "@angular/forms";
 })
 export class SignupPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+              public authService: AuthService) {
   }
 
   onSignup(form: NgForm) {
-    console.log(form.value);
+    this.authService.signup(form.value.email, form.value.password)
+      .then(data => console.log(data))
+      .catch(error => console.log(error))
+    ;
   }
 
 }
